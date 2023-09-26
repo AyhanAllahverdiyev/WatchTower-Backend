6cd251f3341c9f8e62e79f52a8dc240d31b50355
AyhanAllahverdiyev <aykhanallahverdizade2020@gmail.com>
2023-09-26 13:51:36 +0300

    Readable and  dev-friendly update

--- app.js
+++ app.js
@@ -4,28 +4,24 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const nfcDataRoutes = require("./routes/nfc_dataRoutes");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv"); // Use require instead of import
+const dotenv = require("dotenv");
 
+dotenv.config();
 
-
-dotenv.config(); // Load environment variables from .env
-
-const port = process.env.PORT || 3000; 
-const dbURI = process.env.MONGODB_URI; 
+const port = process.env.PORT || 3000;
+const dbURI = process.env.MONGODB_URI;
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
-// connect to mongodb & listen for requests
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
-// register view engine
 app.set("view engine", "ejs");
 
-// middleware & static files
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
@@ -34,7 +30,7 @@ app.use(function (req, res, next) {
   res.locals.res = res;
   next();
 });
-// routes
+
 app.get("/", (req, res) => {
   res.redirect("/logs");
 });
@@ -43,10 +39,8 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-// blog routes
 app.use("/logs", nfcDataRoutes);
 
-// 404 page
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
